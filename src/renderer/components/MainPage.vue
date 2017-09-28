60e6d9cba3f20869762b88277abe34fe62619a56
Oleg.Ovcharenko <Oleg.Ovcharenko>
2017-09-28 19:12:40 +0300

    resize sidebar

--- src/renderer/components/MainPage.vue
+++ src/renderer/components/MainPage.vue
@@ -1,12 +1,12 @@
 <template>
   <!-- <q-layout view="lhh lpr fFf">
-            <div id="left-bar" slot="left">
-              <activity-bar id="abar" class="vertical-top"></activity-bar>
-              <q-side-bar id="sbar" class="vertical-top"></q-side-bar>
-            </div>
-            <status-bar slot="footer"></status-bar>
-          </q-layout> -->
-  <div id="main-container" v-on:mousemove="onMouseMove" v-on="onMouseUp">
+              <div id="left-bar" slot="left">
+                <activity-bar id="abar" class="vertical-top"></activity-bar>
+                <q-side-bar id="sbar" class="vertical-top"></q-side-bar>
+              </div>
+              <status-bar slot="footer"></status-bar>
+            </q-layout> -->
+  <div id="main-container" v-on:mousemove="onMouseMove" v-on:mouseup="onMouseUp" v-bind:style="{gridTemplateColumns: '[activitybarstart] 50px [sidebarstart] ' + sideBarWidth + 'px [sliderstart] 3px [workcontainerstart] 1fr [workcontainerend]'}">
     <div id="abar" v-on:mousedown.left="onToggleSideBar">
       <activity-bar></activity-bar>
     </div>
@@ -34,48 +34,67 @@ export default {
     'q-side-bar': QSideBar,
     'status-bar': StatusBar
   },
+
   data () {
     return {
       seenSideBar: true,
-      gridColumnStart: 4
+      gridColumnStart: 'workcontainerstart',
+      sideBarWidth: 250
     }
   },
+
   mounted () {
     console.log('main-container mounted')
+    const vm = this
+    vm.sideBarWidth = this.$refs.sideBar.offsetWidth
   },
+
   beforeDestroy () {
     console.log('main-container beforeDestroy')
   },
+
   methods: {
     onMouseDown (e) {
       console.log('mouse down')
       const vm = this
       vm.isStartMove = true
-      vm.startWidth = this.$refs.sideBar.offsetWidth
+      vm.startPageX = e.pageX
     },
     onMouseMove (e) {
       const vm = this
-      if (vm.isStartMove) {
-        const width = vm.startWidth + e.pageX
-        vm.qqq = width
+      if (vm.isStartMove === true) {
+        const moveX = e.pageX - vm.startPageX
+        this.sideBarWidth += moveX
+        // ограничение на минимальную ширину сайдбара
+        if (moveX < 0 && this.sideBarWidth < 200) {
+          vm.startPageX = e.pageX + 200 - this.sideBarWidth
+          this.sideBarWidth = 200
+        } else if (e.pageX > vm.$el.offsetWidth - 200) {
+          // ограничение на максимальную ширину сайдбара
+          this.sideBarWidth = this.sideBarWidth - moveX + vm.$el.offsetWidth - 200 - vm.startPageX
+          vm.startPageX = vm.$el.offsetWidth - 200
+        } else {
+          vm.startPageX = e.pageX
+        }
+
+        console.log('mouse move ' + moveX + '= ' + this.sideBarWidth)
       }
     },
     onMouseUp (e) {
       console.log('mouse up')
+      const vm = this
+      vm.isStartMove = undefined
     },
+
+    // переключение видимости сайдбара
     onToggleSideBar (e) {
       console.log('toggle SideBar')
       this.seenSideBar = !this.seenSideBar
-      // var wc = this.$refs.workContainer
-      // wc.hidden = false
       if (this.seenSideBar) {
-        this.gridColumnStart = 4
+        this.gridColumnStart = 'workcontainerstart'
       } else {
-        this.gridColumnStart = 2
+        this.gridColumnStart = 'sidebarstart'
       }
-      // const vm = this
-      // vm.thElm = e.target.parentNode
-      // vm.startOffset = vm.thElm.offsetWidth - e.pageX
     }
   }
 }
@@ -83,34 +102,34 @@ export default {
 
 <style>
 #abar {
-  grid-column: 1;
+  grid-column: activitybarstart;
 }
 
 #sbar {
-  grid-column: 2;
+  grid-column: sidebarstart;
 }
 
 #slider {
-  grid-column: 3;
+  grid-column: sliderstart;
   cursor: col-resize;
 }
 
 #work-container {
-  grid-column-start: 4;
-  grid-column-end: 5;
+  grid-column-start: workcontainerstart;
+  grid-column-end: workcontainerend;
   background-color: rgb(30, 30, 30);
   width: 100%
 }
 
 #footer-container {
   grid-row: 2;
-  grid-column-start: 1;
-  grid-column-end: 5;
+  grid-column-start: activitybarstart;
+  grid-column-end: workcontainerend;
 }
 
 #main-container {
   display: grid;
-  grid-template-columns: 50px 250px 3px 1fr;
+  grid-template-columns: [activitybarstart] 50px [sidebarstart] 250px [sliderstart] 5px [workcontainerstart] 1fr [workcontainerend];
   grid-template-rows: 1fr 22px;
   height: 100%;
 }
