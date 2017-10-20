9ef49b968bdb5293c874c5f4fa1334a552049aaa
Oleg.Ovcharenko <oleg.ovcharenko@labworks.ru>
2017-10-20 17:40:00 +0300

    Tabs

--- src/renderer/components/MainPage/ContentContainer/TopContent.vue
+++ src/renderer/components/MainPage/ContentContainer/TopContent.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="top-content">
-    Main
+    <q-tabs inverted align="justify">
+      <q-tab slot="title" name="tab-1">111</q-tab>
+      <q-tab-pane name="tab-1">Tab 1</q-tab-pane>
+    </q-tabs>
   </div>
 </template>
 
 <script>
+import { QTabs, QTab, QTabPane } from '../../../../../node_modules/quasar-framework';
+
 export default {
+  components: {
+    QTabs,
+    QTab,
+    QTabPane
+  }
 
 }
 </script>
 
 <style>
-
+.q-tabs>.row {
+  margin-left: unset;
+  margin-right: unset;
+}
 </style>
